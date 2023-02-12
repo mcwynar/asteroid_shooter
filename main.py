c907482fbbd771b2d017ef2b720a122b63ef4f12
Michal <mcwynar04@gmail.com>
2023-02-12 19:12:52 +0100

    Creating a meteors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 class Ship(pygame.sprite.Sprite):
     def __init__(self, groups):
@@ -53,6 +54,23 @@ class Laser(pygame.sprite.Sprite):
         self.position += self.direction * dt * self.speed
         self.rect.topleft = (round(self.position.x), round(self.position.y))
 
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, groups, position):
+
+        # basic setup
+        super().__init__(groups)
+        self.image = pygame.image.load('./graphics/meteor.png').convert_alpha()
+        self.rect = self.image.get_rect(center = position)
+        # float based position
+        self.position = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(random.uniform(-0.5, 0.5), 1)
+        self.speed = random.randint(400, 600)
+
+    def update(self):
+        self.position += self.direction * dt * self.speed
+        self.rect.topleft = (round(self.position.x), round(self.position.y))
+
+
 
 # basic setup
 pygame.init()
@@ -67,9 +85,14 @@ background_surf = pygame.image.load('./graphics/background.png').convert()
 # sprite groups
 spaceship_group = pygame.sprite.GroupSingle()
 laser_group = pygame.sprite.Group()
+meteor_group = pygame.sprite.Group()
 
 # sprite creation
 ship = Ship(spaceship_group)
+
+# timer
+meteor_timer = pygame.event.custom_type()
+pygame.time.set_timer(meteor_timer, 400)
 
 # game loop
 while True:
@@ -79,6 +102,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == meteor_timer:
+            meteor_x_pos = random.randint(-100, WINDOW_WIDTH + 100)
+            meteor_y_pos = random.randint(-150, -50)
+            Meteor(meteor_group, (meteor_x_pos, meteor_y_pos))
 
     # delta time
     dt = clock.tick() / 1000
@@ -89,10 +116,12 @@ while True:
     # update
     spaceship_group.update()
     laser_group.update()
+    meteor_group.update()
 
     # graphics
     spaceship_group.draw(display_surface)
     laser_group.draw(display_surface)
+    meteor_group.draw(display_surface)
 
     # draw the frame
     pygame.display.update()
